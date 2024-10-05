421c1bb2b30b389c7a3fcb3451e10402d58270fc
Aneetaom <aneetaperkash@gmail.com>
2024-10-05 15:23:26 -0700

    java-task

---
+++ index.js
@@ -0,0 +1,91 @@
+var userinput = prompt(`
+Celebration of festivals:
+
+ HAPPY EID
+
+ MERRY CHRISTMAS
+
+ HAPPY DIWALI
+
+ HAPPY HOLLY
+
+ HAPPY NOVRATIRY`)
+
+if (userinput === 'happy eid'){
+        document.write(`<div class="card justify-content-center" style="width: 20rem; height: 25rem">
+  <img src="Eid-ul-Adha-Mubarak-GIF.gif" style="width: 20rem; height: 15rem " alt="...">
+  <div class="card-body">
+ 
+  <h4 class="text-center"><b>HAPPY EID</b></h4> 
+   <p class="card-text text-center">Eid is a radiant celebration of faith,
+          gratitude, and unity, where hearts come
+           together to embrace the blessings of peace
+            and compassion 
+          </p>
+  
+   
+  </div>
+</div>`)
+}
+else if(userinput === 'merry christmas'){
+    document.write(`<div class="card justify-content-center" style="width: 20rem; height: 25rem">
+        <img src="9240ef9898db2a3815cc9308d8083479.gif" style="width: 20rem; height: 15rem " alt="...">
+        <div class="card-body">
+        <h4 class="text-center"><b> MERRY CHRISTMAS</b></h4>  
+        
+         <p class="card-text text-center">Christmas is a magical time when joy fills the air, love warms the soul, and the spirit of giving shines brightly in every heart
+    </p>
+        </div>
+      </div>`)
+    }
+    else if(userinput === 'happy diwali'){
+        document.write(`<div class="card justify-content-center" style="width: 20rem; height: 25rem">
+            <img src="damlbd6-36f164c3-d15f-4eb8-8c93-e1e74c5518c6.gif" style="width: 20rem; height: 15rem " alt="...">
+            <div class="card-body">
+            <h4 class="text-center"><b>HAPPY DIWALI</b></h4> 
+            
+             <p class="card-text text-center">Diwali is a festival of lights that illuminates our hearts with joy, hope, and the timeless triumph of light over darkness.
+              </p>
+            </div>
+          </div>`)
+        }
+        else if(userinput === 'happy holly'){
+            document.write(`<div class="card justify-content-center" style="width: 20rem; height: 25rem">
+                <img src="holi-gif-video.gif" style="width: 20rem; height: 15rem " alt="...">
+                <div class="card-body">
+                <h4 class="text-center"><b>HAPPY HOLLY</b></h4> 
+                
+                 <p class="card-text text-center">Holi is a vibrant celebration of colors, joy, and the spirit of togetherness, where laughter and love splash across every heart.
+                  </p>
+                </div>
+              </div>`)
+            }
+            else if(userinput === 'happy novratiry'){
+                document.write(`<div class="card justify-content-center" style="width: 20rem; height: 25rem">
+                    <img src="happy-navratri.gif" style="width: 20rem; height: 15rem " alt="...">
+                    <div class="card-body">
+                    <h4 class="text-center"><b>HAPPY NOVRATIRY</b></h4> 
+                    
+                     <p class="card-text text-center">Navratri is a sacred celebration of devotion, strength, and the triumph of good over evil, where the divine feminine energy dances through every soul.
+                      </p>
+                    </div>
+                  </div>`)
+                }
+
+
+
+
+
+
+
+        
+    
+
+   
+
+
+
+
+
+
+
